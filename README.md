f9b3094366efe92de63300f301922f2f93550ff7
xiehuayong <huayong.xie>
2020-12-06 00:43:21 +0800

    init

--- README.md
+++ README.md
@@ -22,3 +22,14 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+###
+components:
+    common  : 公共组件
+    content : 当前项目的公共组件
+
+assets  : 资源
+common  : 公共资源
+    utils : 工具的js
+network : 封装的网络请求
+
--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const Vue =  createApp(App);
+
+Vue.config.productionTip = false;
+
+Vue.use(store).use(router).mount('#app')
